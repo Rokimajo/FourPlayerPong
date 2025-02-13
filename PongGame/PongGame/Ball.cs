75b03ea27e1dd35c8ebf2021140acc850a2ab7dd
Rokimajo <robinvliegtvandaag@gmail.com>
2025-02-13 19:45:45 +0100

    Added random angle for ball

--- PongGame/PongGame/Ball.cs
+++ PongGame/PongGame/Ball.cs
@@ -7,20 +7,21 @@ namespace PongGame;
 
 public class Ball
 {
-    private float _speed = 250f;
+    private float _speed = 3f;
+    private float _angle;
     private Vector2 _position;
-    private Vector2 _angle;
     private Rectangle _rectangle;
     public Ball()
     {
         _position = new Vector2(Globals.ScreenWidth / 2, Globals.ScreenHeight / 2);
         _rectangle = new Rectangle((int)_position.X, (int)_position.Y, 10, 10);
         Random random = new Random();
+        _angle = random.Next(0, 360);
     }
     public void Update(GameTime gameTime)
     {
-        _rectangle.X -= (int)(_speed * (float) gameTime.ElapsedGameTime.TotalSeconds);
-        _rectangle.Y += (int)(_speed * (float) gameTime.ElapsedGameTime.TotalSeconds);
+        _rectangle.X += (int)(_speed * Math.Cos(_angle));
+        _rectangle.Y += (int)(_speed * Math.Sin(_angle));
     }
 
     public void Draw(SpriteBatch spriteBatch)
